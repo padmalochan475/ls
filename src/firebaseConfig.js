725abe80ac20fbf6d39a777598c4e508a75eb20f
Padmalochan Maharana <padmalochan.maharana@tat.ac.in>
2025-12-03 21:15:47 +0530

    Fix: Correct Firebase Storage Bucket URL

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDwGYhgu3Hx_Fdycq3-6tfKNTPYXVZV5ck",
     authDomain: "lams-59998.firebaseapp.com",
     projectId: "lams-59998",
-    storageBucket: "lams-59998.firebasestorage.app",
+    storageBucket: "lams-59998.appspot.com",
     messagingSenderId: "250431971958",
     appId: "1:250431971958:web:aa4db54237825bcd0f7557",
     measurementId: "G-0HN1E0VMRS"
