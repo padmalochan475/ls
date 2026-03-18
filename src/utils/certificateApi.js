7615ab9dd468eed1172f375e7a4e4d99b133a68c
Padmalochan Maharana <padmalochan.maharana@tat.ac.in>
2026-03-18 12:04:50 +0530

    fix: correct untruncated Apps Script URL

--- src/utils/certificateApi.js
+++ src/utils/certificateApi.js
@@ -1,6 +1,6 @@
 import toast from 'react-hot-toast';
 
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzueqxx7_cYkiNrP2YEMEEyCdanWJ88ymQ/exec"; // SET YOUR DEPLOYED URL HERE
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzTuw_0mVLElTncV30QRDmuaDyuikqWdZBmlE6EIfPjJjIgd5gtAY8qEFEanpcWLd6Nhw/exec"; // SET YOUR DEPLOYED URL HERE
 
 export const certApi = {
     async call(action, data = {}) {
